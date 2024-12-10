d5d0ed1b5a67b8b508daa3495568a7a397f86fb4
Marine <marine.schuermans@hotmail.fr>
2024-12-10 15:33:03 +0100

    test resto

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -9,88 +9,116 @@ import FontAwesome6 from 'react-native-vector-icons/FontAwesome';
 export default function HomeScreen({ navigation }) {
     const dispatch = useDispatch()
     const [favorites, setFavorites] = useState(new Set());
+    const [restaurants, setRestaurants] = useState([]);
     const categories = ['Fast food', 'Italien', 'Asiatique', 'Gastronomique'];
     const user = useSelector((state) => state.user.value)
 
     console.log(user.favorite)
 
-    const restaurants = [
-        {
-            id: 1,
-            title: "Le Gourmet",
-            description: "Fine dining experience",
-            rating: 4.5,
-            image: [
-                "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
-                "https://placeholder.com/150x100"
-            ],
-            phoneNumber: "123-456-7890",
-            location: "123 Main St",
-            latitude: 48.8566,
-            longitude: 2.3522,
-            type : 'family'
-        },
-        {
-            id: 2,
-            title: "Saveurs d'Asie",
-            description: 'Description duis aute irure dolor in reprehenderit in volup...',
-            rating: 4.9,
-            image: [
-                "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
-                "https://placeholder.com/150x100"
-            ],
-            phoneNumber: '+33987654321',
-            location: '456 Rue de la Concorde, Paris, France',
-            latitude: 43.7102,
-            longitude: 7.2620,
-            type : 'date'
-        },
-        {
-            id: 3,
-            title: 'Pizza Roma',
-            description: 'Description duis aute irure dolor in reprehenderit in v...',
-            rating: 4.9,
-            image: [
-                "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
-                "https://placeholder.com/150x100"
-            ],
-            phoneNumber: '+33987654321',
-            location: '789 Rue de la Ferme, Paris, France',
-            latitude: 45.7640,
-            longitude: 4.8357,
-            type : 'couple'
-        },
-        {
-            id: 4,
-            title: 'Le Bistrot',
-            description: 'Description duis aute irure dolor in reprehenderit in v...',
-            rating: 4.8,
-            image: [
-                "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
-                "https://placeholder.com/150x100"
-            ],
-            phoneNumber: '+33123456789',
-            location: '123 Rue de la Paix, Paris, France',
-            latitude : 44.8378,
-            longitude : -0.5792,
-            type : 'date'
-        },
-        {
-            id: 5,
-            title: 'Sushi Master',
-            description: 'Description duis aute irure dolor in reprehenderit in v...',
-            rating: 4.0,
-            image: [
-                "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
-                "https://placeholder.com/150x100"
-            ],
-            phoneNumber: '+33123456789',
-            location: '456 Rue de la Concorde, Paris, France',
-            latitude: 48.3904,
-            longitude: -4.4861,
-            type : 'couple'
-        }
-    ];
+    useEffect(() => {
+        const restaurants = async () => {
+            try {
+            
+                const response = await fetch('http://192.168.1.13:3000/findNearbyRestaurants');
+                const restaurantData = await response.json();
+    
+          
+                const formattedRestaurants = restaurantData.map((place, index) => ({
+                    id: index + 1,
+                    title: place.name,
+                    location: place.address,
+                    description: "Ici, bientôt une description",
+                    rating: place.rating,
+                    image: place.photo, 
+                    phoneNumber: place.phoneNumber,
+                }));
+    
+                setRestaurants(formattedRestaurants);
+            } catch (error) {
+                console.error('Error fetching restaurants:', error);
+            }
+        };
+    
+        restaurants();
+    }, []);
+
+    // const restaurants = [
+    //     {
+    //         id: 1,
+    //         title: "Le Gourmet",
+    //         description: "Fine dining experience",
+    //         rating: 4.5,
+    //         image: [
+    //             "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
+    //             "https://placeholder.com/150x100"
+    //         ],
+    //         phoneNumber: "123-456-7890",
+    //         location: "123 Main St",
+    //         latitude: 48.8566,
+    //         longitude: 2.3522,
+    //         type : 'family'
+    //     },
+    //     {
+    //         id: 2,
+    //         title: "Saveurs d'Asie",
+    //         description: 'Description duis aute irure dolor in reprehenderit in volup...',
+    //         rating: 4.9,
+    //         image: [
+    //             "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
+    //             "https://placeholder.com/150x100"
+    //         ],
+    //         phoneNumber: '+33987654321',
+    //         location: '456 Rue de la Concorde, Paris, France',
+    //         latitude: 43.7102,
+    //         longitude: 7.2620,
+    //         type : 'date'
+    //     },
+    //     {
+    //         id: 3,
+    //         title: 'Pizza Roma',
+    //         description: 'Description duis aute irure dolor in reprehenderit in v...',
+    //         rating: 4.9,
+    //         image: [
+    //             "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
+    //             "https://placeholder.com/150x100"
+    //         ],
+    //         phoneNumber: '+33987654321',
+    //         location: '789 Rue de la Ferme, Paris, France',
+    //         latitude: 45.7640,
+    //         longitude: 4.8357,
+    //         type : 'couple'
+    //     },
+    //     {
+    //         id: 4,
+    //         title: 'Le Bistrot',
+    //         description: 'Description duis aute irure dolor in reprehenderit in v...',
+    //         rating: 4.8,
+    //         image: [
+    //             "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
+    //             "https://placeholder.com/150x100"
+    //         ],
+    //         phoneNumber: '+33123456789',
+    //         location: '123 Rue de la Paix, Paris, France',
+    //         latitude : 44.8378,
+    //         longitude : -0.5792,
+    //         type : 'date'
+    //     },
+    //     {
+    //         id: 5,
+    //         title: 'Sushi Master',
+    //         description: 'Description duis aute irure dolor in reprehenderit in v...',
+    //         rating: 4.0,
+    //         image: [
+    //             "https://www.aixenprovencetourism.com/wp-content/uploads/2013/07/ou_manger-1920x1080.jpg",
+    //             "https://placeholder.com/150x100"
+    //         ],
+    //         phoneNumber: '+33123456789',
+    //         location: '456 Rue de la Concorde, Paris, France',
+    //         latitude: 48.3904,
+    //         longitude: -4.4861,
+    //         type : 'couple'
+    //     }
+    // ];
 
     // Verifier si le user est connecté via la présence ou non d'un token
     let isConnected = false
